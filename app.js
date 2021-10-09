961c3e4b05606442a17f34e6c466c625f27a841d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-09 06:18:02 +0300

    some advancements

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //   "https://sheltered-scrubland-49038.herokuapp.com/http://career.dijitalgaraj.com/hash/murat-can-yuksel-14382",
+//get the hash with PUT method
 fetch(
   "https://sheltered-scrubland-49038.herokuapp.com/http://career.dijitalgaraj.com/hash/murat-can-yuksel-14382",
   {
@@ -14,5 +15,15 @@ fetch(
   })
   .then((data) =>
     // this is the data we get after putting our data,
-    console.log(data)
+    // console.log(data.hash.length / 32) results in 17
+    //which means I have 17 substrings at hand
+
+    console.log(data.hash)
   );
+//hash your own email address
+// window.addEventListener("load", function () {
+//   var strHash = md5("muratcanyukselpro@gmail.com");
+//   alert("The MD5 hash of the tutsplus string is:" + strHash);
+// });
+var passhash = CryptoJS.MD5("muratcanyukselpro@gmail.com").toString();
+console.log(passhash);
